0d48294bb3b22ad059219572da3013a238231eb3
shuanshuan030913 <skypainter59768@gmail.com>
2020-09-17 16:47:56 +0800

    feat. google-map-react

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../assets/scss/main.scss';
+import GoogleMapReact from 'google-map-react';
+import { GOOGLE_MAP } from '../../assets/key'
 
-function Map() {
+function TimeTable() {
   return (
-    <div className="map_page">
-      <div className="timetable">
-        <nav>
-          <ul>
-            <li>nav</li>
-            <li>nav</li>
-            <li>nav</li>
-          </ul>
-        </nav>
-        <div className="timetable_list">
-          <div className="schedule">123</div>
-          <div className="schedule">123</div>
-          <div className="schedule">123</div>
-        </div>
+    <div className="timetable">
+      <nav>
+        <ul>
+          <li>nav</li>
+          <li>nav</li>
+          <li>nav</li>
+        </ul>
+      </nav>
+      <div className="timetable_list">
+        <div className="schedule">123</div>
+        <div className="schedule">123</div>
+        <div className="schedule">123</div>
       </div>
     </div>
   );
 }
 
-export default Map;
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 20.6778226,
+      lng: 116.7253489,
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <div className="map_page">
+        <TimeTable />
+        <div className="google-map-section">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: GOOGLE_MAP.KEY }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            {/* 標記地點用 */}
+            <Pin
+              lat={20.6778226}
+              lng={116.7253489}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Pin() {
+  return (
+    <div className="pin">雨晴推薦的點</div>
+  );
+}
+
+export default SimpleMap;
