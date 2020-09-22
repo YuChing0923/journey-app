df28e0e6198beafd0e4d137b529451642a9c93df
shuanshuan030913 <skypainter59768@gmail.com>
2020-09-22 23:01:17 +0800

    state 地點切換

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,20 +3,55 @@ import '../../assets/scss/main.scss';
 import GoogleMapReact from 'google-map-react';
 import { GOOGLE_MAP } from '../../assets/key'
 
-function TimeTable() {
+const TimeTable = (props) => {
   return (
     <div className="timetable">
       <nav>
         <ul>
-          <li>nav</li>
-          <li>nav</li>
-          <li>nav</li>
+          <li className="active">Day1</li>
+          <li>Day2</li>
+          <li>Day3</li>
         </ul>
       </nav>
       <div className="timetable_list">
-        <div className="schedule">123</div>
-        <div className="schedule">123</div>
-        <div className="schedule">123</div>
+        <div className="schedule" onClick={() => {
+          props.moveMark({
+            lat: 24.7295481,
+            lng: 120.8764177,
+            landmark: '崎頂隧道文化公園',
+          })
+        }}>
+          <div><div className="time">10:00</div></div>
+          <div>
+            <div className="title">崎頂隧道文化公園</div>
+            <p className="content">第一站！</p>
+          </div>
+        </div>
+        <div className="schedule" onClick={() => {
+          props.moveMark({
+            lat: 24.4138864,
+            lng: 120.7683948,
+            landmark: '建中國小3D彩繪階梯',
+          })
+        }}>
+          <div><div className="time">12:00</div></div>
+          <div>
+            <div className="title">建中國小3D彩繪階梯</div>
+            <p className="content">順便吃飯吃飯吃飯</p>
+          </div>
+        </div>
+        <div className="schedule" onClick={() => {
+          props.moveMark({
+            lat: 24.3584335,
+            lng: 120.7716469,
+            landmark: '龍騰斷橋(魚藤坪斷橋)',
+          })
+        }}>
+          <div><div className="time">14:00</div></div>
+          <div>
+            <div className="title">龍騰斷橋(魚藤坪斷橋)</div>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -24,28 +59,53 @@ function TimeTable() {
 
 class SimpleMap extends Component {
   static defaultProps = {
-    center: {
-      lat: 20.6778226,
-      lng: 116.7253489,
-    },
-    zoom: 11
+    zoom: 17
   };
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: {
+        lat: 24.7295481,
+        lng: 120.8764177,
+      },
+      currentlat: 24.7295481,
+      currentlng: 120.8764177,
+      currentMark: '崎頂隧道文化公園',
+    }
+  }
+  
+  moveMark = (data) => {
+    this.setState({
+      currentlat: data.lat,
+      currentlng: data.lng,
+      currentMark: data.landmark,
+      center: {
+        lat: data.lat,
+        lng: data.lng,
+      },
+    })
+  }
 
   render() {
+    const { currentlat, currentlng, currentMark } = this.state;
     return (
       <div className="map_page">
-        <TimeTable />
+        <TimeTable {...{
+          moveMark: data => this.moveMark(data),
+        }}/>
         <div className="google-map-section">
           <GoogleMapReact
             bootstrapURLKeys={{ key: GOOGLE_MAP.KEY }}
-            defaultCenter={this.props.center}
+            defaultCenter={this.state.center}
             defaultZoom={this.props.zoom}
           >
-            {/* 標記地點用 */}
             <Pin
-              lat={20.6778226}
-              lng={116.7253489}
-              text="My Marker"
+              {...{
+                lat: currentlat,
+                lng: currentlng,
+                landmark: currentMark,
+              }}
             />
           </GoogleMapReact>
         </div>
@@ -54,9 +114,9 @@ class SimpleMap extends Component {
   }
 }
 
-function Pin() {
+const Pin = (props) => {
   return (
-    <div className="pin">雨晴推薦的點</div>
+    <div className="pin">{props.landmark}</div>
   );
 }
 
