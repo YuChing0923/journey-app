6cc4c955781408573580f593558b4a320d366245
shuanshuan030913 <skypainter59768@gmail.com>
2020-10-17 15:49:04 +0800

    maker-hover to list

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,59 +3,63 @@ import '../../assets/scss/main.scss';
 import GoogleMapReact from 'google-map-react';
 import moment from 'moment';
 import { GOOGLE_MAP } from '../../assets/key';
-
-let data = [
-  {
-    id: 1,
-    date: '2020/09/28 10:00:00',
-    landmark: '崎頂隧道文化公園',
-    lat: 24.7295481,
-    lng: 120.8764177,
-    content: '第一站！',
-  },
-  {
-    id: 2,
-    date: '2020/09/28 12:00:00',
-    landmark: '建中國小3D彩繪階梯',
-    lat: 24.4138864,
-    lng: 120.7683948,
-    content: '',
-  },
-  {
-    id: 3,
-    date: '2020/09/28 14:00:00',
-    landmark: '龍騰斷橋(魚藤坪斷橋)',
-    lat: 24.3584335,
-    lng: 120.7716469,
-    content: '',
-  },
-  {
-    id: 4,
-    date: '2020/09/28 15:00:00',
-    landmark: '路思義教堂',
-    lat: 24.1788952,
-    lng: 120.5983652,
-    content: '',
-  },
-  {
-    id: 5,
-    date: '2020/09/28 20:00:00',
-    landmark: '望高寮夜景公園',
-    lat: 24.1435843,
-    lng: 120.5791395,
-    content: '',
-  },
-]
-
+import MapMaker from './mapMaker.js';
+import data from './temp_data.js';
 
 const SimpleMap = ({zoom}) => {
   const [center, setCenter] = useState({
         lat: 23.546162,
         lng: 120.6402133,
       });
-  const [currentlat, setCurrentlat] = useState(24.7295481);
-  const [currentlng, setCurrentlng] = useState(120.8764177);
-  const [currentMark, setCurrentMark] = useState('崎頂隧道文化公園');
+  const [currentId, setCurrentId] = useState(false);
+  const [hoverDom, setHoverDom] = useState(false);
+
+  // google api 路徑繪製
+  const handleApiLoaded = (map, maps, data) => {
+    if (map) {
+      const directionsService = new maps.DirectionsService();
+      const directionsDisplay = new maps.DirectionsRenderer();
+      let waypoints = [];
+      if (data.length > 2) {
+        for (let i = 1; i < data.length - 1; i++) {
+          waypoints.push({location: data[i].landmark})
+        }
+      }
+
+      directionsService.route({
+        origin: data[0].landmark,
+        destination: data[data.length - 1].landmark,
+        waypoints: waypoints,
+        travelMode: 'DRIVING'
+      }, (response, status) => {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+          const routePolyline = new maps.Polyline({
+            path: response.routes[0].overview_path,
+            strokeColor: "#187afb",
+            strokeWeight: 3,
+            geodesic: true,
+          });
+          routePolyline.setMap(map);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+          }
+        }
+      );
+    }
+  };
+
+  const onChildClick = () => {
+    console.log('onChildClick')
+  }
+  const onChildMouseEnter = (index, childProps) => {
+    setCurrentId(childProps.id);
+    console.log('onChildMouseEnter');
+  }
+  const onChildMouseLeave = () => {
+    setCurrentId(false);
+    console.log('onChildMouseLeave')
+  }
 
   return (
     <div className="map_page">
@@ -65,10 +69,8 @@ const SimpleMap = ({zoom}) => {
             lat: data.lat,
             lng: data.lng,
           });
-          setCurrentlat(data.lat);
-          setCurrentlng(data.lng);
-          setCurrentMark(data.landmark);
         },
+        currentId: currentId,
       }}/>
       <div className="google-map-section">
         <GoogleMapReact
@@ -76,15 +78,20 @@ const SimpleMap = ({zoom}) => {
           defaultCenter={center}
           defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
+          onChildClick={onChildClick}
+          onChildMouseEnter={onChildMouseEnter}
+          onChildMouseLeave={onChildMouseLeave}
           onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps, data)}
         >
-        {/* data.map(d => <Pin
+        { data.map(d => <MapMaker
             {...{
+              id: d.id,
               lat: d.lat,
               lng: d.lng,
-              landmark: d.landmark,
+              landmark: d.landmark[0],
+              hoverDom: hoverDom,
             }}
-          />) */}
+          />)}
         </GoogleMapReact>
       </div>
     </div>
@@ -93,6 +100,7 @@ const SimpleMap = ({zoom}) => {
 SimpleMap.defaultProps = {
     zoom: 8
 }
+
 
 
 // 時間軸
@@ -108,7 +116,7 @@ const TimeTable = (props) => {
       </nav>
       <div className="timetable_list">
         { data.map(d =>
-            <div key={d.id} className="schedule" onClick={() => {
+            <div key={d.id} className={`schedule ${props.currentId === d.id ? 'maker_hover' : ''}`} onClick={() => {
               props.moveMark({
                 lat: d.lat,
                 lng: d.lng,
@@ -128,53 +136,8 @@ const TimeTable = (props) => {
     </div>
   );
 }
-
-// google api
-const handleApiLoaded = (map, maps, data) => {
-  if (map) {
-    // 景點繪製
-    for (let d of data) {
-      let marker = new maps.Marker({
-        position: { lat: d.lat, lng: d.lng},
-        map,
-        title: d.landmark,
-      });
-    }
-
-    // 路徑繪製
-    const directionsService = new maps.DirectionsService();
-    const directionsDisplay = new maps.DirectionsRenderer();
-    let waypoints = [];
-    if (data.length > 2) {
-      for (let i = 1; i < data.length - 1; i++) {
-        waypoints.push({location: data[i].landmark})
-      }
-    }
-
-    directionsService.route({
-      origin: data[0].landmark,
-      destination: data[data.length - 1].landmark,
-      waypoints: waypoints,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-        const routePolyline = new maps.Polyline({
-          path: response.routes[0].overview_path
-        });
-        routePolyline.setMap(map);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-        }
-      }
-    );
-  }
-};
-
-const Pin = (props) => {
-  return (
-    <div className="pin">{props.landmark}</div>
-  );
+const handleMarkerClick = (maps, marker) => {
+  console.log('click', marker);
 }
 
 export default SimpleMap;
---
+++ src/components/Map/mapMaker.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+const MapMaker = (props) => {
+  return (
+    <div className="pin">{props.landmark}</div>
+  );
+}
+
+export default MapMaker;
